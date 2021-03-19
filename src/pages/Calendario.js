0bdd45d2e65c0d7650b2a80009b1447f039df2f9
Rodrigo do Nascimento Siqueira <rodignascimento@gmail.com>
2021-03-18 22:29:16 -0300

    v1.06 Calendario feito

--- src/pages/Calendario.js
+++ src/pages/Calendario.js
@@ -1,14 +1,54 @@
-import React from "react";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Navbar from "../components/Navbar/Navbar";
 import "../styles.css";
+import {
+  Inject,
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  EventSettingsModel
+} from "@syncfusion/ej2-react-schedule";
 
-export default function Calendario() {
-  return (
-    <div>
-      <Navbar />
-      <Sidebar />
-      <h1>Calendario</h1>
-    </div>
-  );
+import { DataManager, WebApiAdaptor } from "@syncfusion/ej2-data";
+
+export default class Calendario extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.data = [
+      {
+        Id: 2,
+        Subject: "Aula",
+        StartTime: new Date(2021, 2, 15, 10, 0),
+        EndTime: new Date(2021, 2, 18, 12, 30)
+      },
+      {
+        Id: 3,
+        Subject: "to",
+        StartTime: new Date(2021, 2, 16, 9, 0),
+        EndTime: new Date(2021, 2, 25, 12, 30)
+      }
+    ];
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Sidebar />
+        <ScheduleComponent
+          currentView="Month"
+          height="550px"
+          width="80%"
+          style={{ margin: "auto", marginTop: "50px" }}
+          eventSettings={{ dataSource: this.data }}
+        >
+          <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+        </ScheduleComponent>
+      </div>
+    );
+  }
 }
